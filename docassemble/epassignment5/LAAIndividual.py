eba651a67914d4d48e374cc880595f23768eddd1
Erin Peters <ebpeters@ualberta.ca>
2019-10-16 14:04:45 -0600

    Erin assignment 5

--- docassemble/epassignment5/LAAIndividual.py
+++ docassemble/epassignment5/LAAIndividual.py
@@ -1,8 +1,8 @@
-from docassemble.base.util import Individual
+from docassemble.base.util import Individual, DAObject, Value
 
 class LAAIndividual(Individual):
   def init(self, *pargs, **kwargs):
-    self.initializeAttribute('annual_income', DAObject)
-    self.initializeAttribute('monthly_income', DAObject)
-    self.initializeAttribute('assets', DAObject)
+    self.initializeAttribute('annual_income', Value)
+    self.initializeAttribute('monthly_income', Value)
+    self.initializeAttribute('assets', Value)
     super(LAAIndividual, self).init(*pargs, **kwargs)
